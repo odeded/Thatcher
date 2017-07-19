fcc301a3a42480c70a09ab53378853dc37226946
odeded <odedsh@yahoo.com>
2017-07-19 22:53:43 +0300

    dropbox-c

---
+++ externals/Dropbox-C/Dropbox/include/dropboxOAuth.h
@@ -0,0 +1,45 @@
+/*!
+ * \file    dropboxOAuth.h
+ * \brief   OAuth library for dropbox library.
+ * \author  Adrien Python
+ * \version 1.0
+ * \date    29.10.2013
+ */
+
+#ifndef DROPBOX_OAUTH_H
+#define DROPBOX_OAUTH_H
+
+#include <stdbool.h>
+#include <openssl\e_os2.h>
+#include "dropbox.h"
+#include "dropboxUtils.h"
+
+#define ssize_t ossl_ssize_t
+
+typedef union {
+    void* ptr;
+    char* str;
+    int value;
+} drbOptArg;
+
+/*!
+ * \struct  drbClient
+ * \breif   Client data used the whole time.
+ */
+struct drbClient{
+    drbOAuthToken c;
+    drbOAuthToken t;
+    drbOptArg defaultOptions[DRBOPT_END];
+};
+
+int drbOAuthGet(drbClient* cli, const char* url, void* data, void* writeFct, int timeout);
+int drbOAuthPost(drbClient* cli, const char* url, void* data, void* writeFct, int timeout);
+
+int drbOAuthGetFile(drbClient* cli, const char* url, void* data, void* writeFct, char** answer, int timeout);
+int drbOAuthPostFile(drbClient* cli, const char *url, void* data,
+                     ssize_t (*readFct)(void *, size_t , size_t , void *),
+                     char** answer, int timeout);
+char *drbEncodePath(const char *string);
+bool drbParseOauthTokenReply(const char *answer, char **key, char **secret);
+
+#endif /* DROPBOX_OAUTH_H */
